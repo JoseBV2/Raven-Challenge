523ad03a51787dc10bdca45559010c0585ba1a5f
Jose Bustamante <josebusta2001@gmail.com>
2024-11-24 10:34:54 -0500

    Initial Commit

---
+++ Raven Challenge/Raven_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Raven_ChallengeApp.swift
+//  Raven Challenge
+//
+//  Created by Jóse Bustamante on 11/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Raven_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Raven ChallengeTests/Raven_ChallengeTests.swift
@@ -0,0 +1,16 @@
+//
+//  Raven_ChallengeTests.swift
+//  Raven ChallengeTests
+//
+//  Created by Jóse Bustamante on 11/24/24.
+//
+
+import Testing
+
+struct Raven_ChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
